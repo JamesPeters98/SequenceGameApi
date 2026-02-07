63d0039f3999a74d89f84926c4f461a856703b12
James Peters <jameslfc19@gmail.com>
2026-02-07 17:13:44 +0000

    Add dark mode to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
+import { ThemeProvider } from "@/components/theme-provider";
 import { HomePage } from "@/features/game/pages/HomePage";
 import { LobbyPage } from "@/features/game/pages/LobbyPage";
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/lobby/:gameUuid" element={<LobbyPage />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/lobby/:gameUuid" element={<LobbyPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
